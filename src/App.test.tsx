55819b9f2a6842f980c27d3d670b686124419f2f
Florian Georg <florian.alexander.georg@gmail.com>
2021-01-10 14:44:29 +0100

    First for Azure Static Webapp

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('checks for Home side menu link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const menu_item_element = screen.getByText(/Home/i);
+  expect(menu_item_element).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
+import './App.less';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Image, Layout, Menu } from 'antd';
+
+// see https://ant.design/components/icon/
+import {
+  UploadOutlined, UserOutlined,
+  VideoCameraOutlined, AudioFilled,
+  LinkOutlined, MedicineBoxTwoTone,
+  CameraOutlined,
+  BulbOutlined,
+  ControlOutlined,
+  ExperimentOutlined,
+  FilePdfOutlined,
+  FileWordOutlined,
+  FilePptOutlined,
+  FileUnknownOutlined,
+  HomeOutlined,
+  IdcardOutlined,
+  MessageOutlined,
+  LoadingOutlined,
+  NodeIndexOutlined,
+  PartitionOutlined,
+  PaperClipOutlined,
+  RobotOutlined,
+  SettingOutlined,
+  SoundOutlined,
+  StarOutlined,
+  StarFilled,
+  TagsOutlined,
+  TranslationOutlined
+
+
+} from '@ant-design/icons';
+
+
+
+
+const { Header, Content, Footer, Sider } = Layout;
+
+
+
+
+const App: FC = () => (
+  <Layout>
+    <Sider
+      theme="light"
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={broken => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="logo">
+        <Image src="logo.png" ></Image>
+      </div>
+
+      <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
+        <Menu.Item key="1" icon={<HomeOutlined />}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="2" icon={<CameraOutlined />}>
+          Scan a picture
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UploadOutlined />}>
+          Upload a document
+        </Menu.Item>
+        <Menu.Item key="4" icon={<AudioFilled />}>
+          Transcribe Audio
+        </Menu.Item>
+        <Menu.Item key="5" icon={<LinkOutlined />}>
+          Analyse Web Article
+        </Menu.Item>
+
+
+      </Menu>
+    </Sider>
+    <Layout style={{ height: '100vh' }}>
+      <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+
+      <Content style={{ margin: '24px 16px 0' }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 360, height: "100%" }}>
+          content
+        </div>
+      </Content>
+
+      <Footer style={{ textAlign: 'center' }}>© 2021 by MedJargonBuster Team <a href="https://medjargonbuster.com">https://medjargonbuster.com</a></Footer>
+    </Layout>
+  </Layout>);
+
 
 export default App;
